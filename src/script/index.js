e5f08856327de52d5a12b65b6a4b4e4ec092535a
Gabriel Cemcher <gabrielrenan82@hotmail.com>
2024-08-01 17:36:26 -0300

    adicionando o arquivo index.html e a pasta src com os arquivos do projeto

---
+++ src/script/index.js
@@ -0,0 +1,20 @@
+const inputsForm = document.querySelectorAll('.input-form');
+const enviarForm = document.querySelector('.btn-enviar');
+const paragrafoObrigatorio = document.querySelectorAll('.paragrafo-obrigatorio');
+
+
+enviarForm.addEventListener('click', () =>{
+    event.preventDefault();
+    
+    inputsForm.forEach((inputsForm, index) => {
+        if(inputsForm.value === ""){
+            inputsForm.classList.add('campo-obrigatorio');
+            inputsForm.classList.remove('campo-preenchido');
+            paragrafoObrigatorio[index].classList.add('mostra-obrigatorio');
+        }else {
+            inputsForm.classList.add('campo-preenchido')
+            inputsForm.classList.remove('campo-obrigatorio')
+            paragrafoObrigatorio[index].classList.remove('mostra-obrigatorio');
+        }
+    });
+});
